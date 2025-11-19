455b594f1ffa0059ca17091a60d4ccfeb2fd386c
LorienYang <yangluoying19277904322@outlook.com>
2025-11-19 22:00:26 +0800

    新增图片

--- src/assets/config/images.ts
+++ src/assets/config/images.ts
@@ -3,5 +3,4 @@ const modules = import.meta.glob('../imgs/*.{png,jpg,jpeg,gif,webp}', {
     import: 'default'
 });
 
-// 让 TS 知道这些都是 string
 export const imageList = Object.values(modules) as string[];
